befc3799f232f5ed0b0f7a7c2f5d3f079eed6613
Nicolò Ribaudo <nribaudo@igalia.com>
2026-08-01 16:32:27 +0200

    Delete old publish script

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -xe
-
-ecmarkup spec/index.html index.next
-git checkout gh-pages
-mv index.next index.html
-git add index.html
-git commit -m 'Publish'
-git push origin gh-pages
-git checkout master
